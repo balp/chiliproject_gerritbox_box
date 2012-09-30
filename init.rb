43979740a450c478ea72ce491462e1b9d504728d
Anders Arnholm <Anders@Arnholm.se>
2012-09-30 10:16:03 +0200

    First basic test, connects to gerrit on 127.0.0.1 using ssh and list open issues.

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :chiliproject_gerritbox_box do
+  name 'Chiliproject Gerrit My Page plugin'
+  author 'Anders Arnholm'
+  description 'To add a box in my pages with open gerrit reviews'
+  version '0.0.1'
+  url 'https://github.com/balp/chiliproject_gerritbox_box'
+  author_url 'https://github.com/balp/'
+end
